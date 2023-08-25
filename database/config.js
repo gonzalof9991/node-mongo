86bfcbdbb1d4da35a938a0d17d332846d00466c4
gonzalo9991r <go.rojasf@duocuc.cl>
2023-08-25 11:31:59 -0400

    FIX: deploy

--- database/config.js
+++ database/config.js
@@ -7,6 +7,7 @@ const dbConnection = async () => {
         });
         console.log('DB online');
     } catch (error) {
+        console.log(process.env.MONGODB_CNN);
         console.log(error);
         throw new Error('Error a la hora de iniciar la BD ver logs');
     }
